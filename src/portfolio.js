09be003ad878bea62d3e3fa83a19daa103fade40
Alex Aritan <cokethenwpepsi@gmail.com>
2019-10-13 14:38:10 -0400

    Added ability to get a new access token if your refreshtoken and clientid are valid

--- src/portfolio.js
+++ src/portfolio.js
@@ -4,15 +4,22 @@ import rp from "request-promise";
 
 export default class {
 
-  constructor(accessToken){
-    this.auth = `Bearer ${accessToken}`;
+  //Arguments:
+  //accessToken - required; needed to authenticate with every request to the API.
+  //refreshToken - optional; needed if the user would like to be able to refresh their accessToken programmatically.
+  //clientId - optional; needed if the user would like to be able to refresh their accessToken programmatically.
+  constructor(accessToken, refreshToken, clientId){
+    //TODO solve the problem of the refreshToken expiring every 90 days - a limit imposed by the TDA API.
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+    this.clientId = clientId;
     this.baseUrl = "https://api.tdameritrade.com/v1/";
   }
 
   async getTicker(ticker){
     //Validate ticker input.
-    if(!ticker) throw new Error("Ticker must be provided as a string.");
-    if(typeof ticker !== "string") throw new Error("Ticker must be provided as a string.");
+    if(!ticker) throw new Error({message: "Ticker must be provided as a string."});
+    if(typeof ticker !== "string") throw new Error({message: "Ticker must be provided as a string."});
 
     //Set up options for the request.
     const tck = ticker.toLowerCase();
@@ -20,7 +27,7 @@ export default class {
       method: "GET",
       uri: `${this.baseUrl}marketdata/${tck}/quotes`,
       headers: {
-        Authorization: this.auth
+        Authorization: `Bearer ${this.accessToken}`
       }
     };
 
@@ -29,7 +36,7 @@ export default class {
       return await rp(options);
     }
     catch(e){
-      return e;
+      throw e;
     }
   }
 
@@ -79,11 +86,32 @@ export default class {
     return {};
   }
 
-  refreshAccessToken(refreshToken, clientId){
-    //TODO make a request to the endpoint that gets you a new access token.
+  async refreshAccessToken(){
+    //Set up options for the request.
+    const options = {
+      method: "POST",
+      uri: "https://api.tdameritrade.com/v1/oauth2/token",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      form: {
+        "grant_type": "refresh_token",
+        "refresh_token": this.refreshToken,
+        "client_id": this.clientId
+      }
+    };
 
-    const accessToken = "";
-    this.auth = `Bearer ${accessToken}`;
+    //Make the request to the API.
+    try{
+      const response = await rp(options);
+      const parsedResponse = JSON.parse(response);
+      if(parsedResponse.access_token) this.accessToken = parsedResponse.access_token;
+      else throw new Error({message: "Could not refresh access token."});
+      return parsedResponse;
+    }
+    catch(e){
+      throw e;
+    }
   }
 
 }
